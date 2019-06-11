cd797b7d44fe61ddf8933292c8f1a69db8f35626
sudhirgudapati1998 <51699456+sudhirgudapati1998@users.noreply.github.com>
2019-06-11 14:36:00 +0530

    Create pnz.c

---
+++ pnz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+    printf("poitive");
+    else if(n==0)
+    printf("zero");
+    else
+    printf("negative");
+    return 0;
+}
